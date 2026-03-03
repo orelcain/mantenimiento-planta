dbdf9ce564e29eb1689b42ae2c1b65137d0b6d15
orelcain <orelcain@hotmail.com>
2026-03-03 12:31:56 -0300

    ok

--- apps/pwa/src/constants/version.ts
+++ apps/pwa/src/constants/version.ts
@@ -11,7 +11,7 @@ export const APP_VERSION = '2.69.19' as const
 /**
  * Fecha de la versión actual
  */
-export const VERSION_DATE = '2026-03-02' as const
+export const VERSION_DATE = '2026-03-03' as const
 
 /**
  * Nombre de la versión (opcional)
